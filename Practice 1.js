7b64d884c6592a01daf4e40c4694f3c80b14e96b
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-08-02 22:09:22 +0530

    Practice set

---
+++ Practice 1.js
@@ -0,0 +1,7 @@
+// create a variable of type string and add a number to it
+let a="Dhruv"
+let b=6
+console.log(a+b)
+//use typeof operator to find the datatype of the string in the above code
+console.log(typeof (a+b))
+
