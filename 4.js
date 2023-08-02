83fb5182e5f5c1aeae11fa9bb0b46fdc4aedae29
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-08-02 21:17:48 +0530

    Primitive and Non-Primitive  Datatypes

---
+++ 4.js
@@ -0,0 +1,26 @@
+//PRIMITIVE DATA TYPES
+//nn bb ss u
+let a=null;
+let b=345;
+let c=true; //it can be false 
+let d=BigInt("567");
+let e="Dhruva"
+let f=Symbol("I am a nice symbol")
+let g=undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+// totally there are 7 types of data types
+// null , number, boolean,byte,string,symbol,undefined
+
+// NON PRIMITIVE DATA TYPES
+// objects
+const item ={
+    "dhruv":true,
+    "sharath":false,
+    "Jayesh":67,
+    "Tejas":undefined
+}
+console.log(item["dhruv"])
+console.log(item["sharath"])
+console.log(item["Jayesh"])
+console.log(item["Tejas"])
