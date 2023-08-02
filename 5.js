1cbbea542369f1253ad1271f76ddf3fb989bd070
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-08-02 22:26:51 +0530

    Operators and Expressions

---
+++ 5.js
@@ -0,0 +1,12 @@
+console.log("Operators in JavaScript")
+let a=45;
+let b=5;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+console.log("a%b=",a%b)
+console.log("a++",a++)
+console.log("a",a)
+console.log("a--",a--)
