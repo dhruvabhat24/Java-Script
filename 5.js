58ba251b9d74cd15c30f2d1a6dc863a2b6af5234
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-08-03 18:15:57 +0530

    Operators and Expressions

--- 5.js
+++ 5.js
@@ -1,3 +1,4 @@
+//Arihmetic Operators
 console.log("Operators in JavaScript")
 let a=45;
 let b=5;
@@ -10,3 +11,23 @@ console.log("a%b=",a%b)
 console.log("a++",a++)
 console.log("a",a)
 console.log("a--",a--)
+console.log("++a",++a)
+//assignment operators
+let assignment=1
+assignment +=5
+console.log(assignment)
+//comaprision Operators
+let comp1=6;
+let comp2="6";
+console.log("comp1==comp2",comp1==comp2)
+console.log("comp1!=comp2",comp1!=comp2)
+console.log("comp1===comp2",comp1===comp2)
+console.log("comp1!==comp2",comp1!==comp2)
+
+// Logical Operators
+let x=5;
+let y=10;
+console.log(x<y && x==5)
+console.log(x<y || y==5)
+console.log(!false)
+console.log(!true)
