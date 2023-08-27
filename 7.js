3bdfd88251de3db92175f0b022a9e926488bb66a
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-08-27 17:49:30 +0530

    for loop

--- 7.js
+++ 7.js
@@ -1,3 +1,17 @@
-for(let i=0; i<5;i++){
-    console.log(i)
+//let sum = 0;
+//let n = prompt("Enter the value of n:");
+//n =Number.parseInt(n);
+//for(let i=0; i<n;i++){
+ //   sum+=i
+//}
+//console.log("Sum of first "+n+" natural number is: "+sum);
+// For in Loop
+let obj ={
+    dhruva:30,
+    rahul:20,
+    manoj:40,
+    ramesh:50
+}
+for(let a in obj){
+    console.log("marks of "+a+" is "+obj[a])
 }
