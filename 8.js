2d1da90d6d51653feb1d700c2e3c6ac90546c4b1
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-10-13 13:51:14 +0000

    while loop

--- 8.js
+++ 8.js
@@ -1,7 +1,16 @@
-let a = prompt("Enter the value of n")
+// While loop
+let n =prompt("Enter a number of n: ");
 n = Number.parseInt(n)
 let i=0;
 while(i<n){
-    console.log(i);
-    i++
+  console.log(i);
+  i++;
 }
+// Do while loop
+let m =prompt("Enter a number of m: ");
+m = Number.parseInt(m)
+let j=0;
+do{
+  console.log(j);
+  j++;
+}while(j<m)
